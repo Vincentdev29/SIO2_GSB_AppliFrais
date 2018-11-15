3ae5020c63513c71c2f052380798af17a9e2ec00
Aubry LEDUCQ <aleducq@sio.chaptalqper>
2018-11-15 16:16:09 +0100

    Connexion espace comptable suite

--- application/models/a_comptable.php
+++ application/models/a_comptable.php
@@ -35,27 +35,10 @@ class A_visiteur extends CI_Model {
 		$this->templates->load('t_comptable', 'v_visAccueil');
 	}
 
-	/**
-	 * Liste les fiches existantes du visiteur connecté et
-	 * donne accès aux fonctionnalités associées
-	 *
-	 * @param $idVisiteur : l'id du visiteur
-	 * @param $message : message facultatif destiné à notifier l'utilisateur du résultat d'une action précédemment exécutée
-	*/
-	public function mesFiches ($idVisiteur, $message=null)
-	{	// TODO : s'assurer que les paramètres reçus sont cohérents avec ceux mémorisés en session
-
-		$idVisiteur = $this->session->userdata('idUser');
-
-		$data['notify'] = $message;
-		$data['mesFiches'] = $this->dataAccess->getFiches($idVisiteur);
-		$this->templates->load('t_visiteur', 'v_visMesFiches', $data);
-	}
-
-	/**
+  /**
 	 * Présente le détail de la fiche sélectionnée
 	 *
-	 * @param $idVisiteur : l'id du visiteur
+	 * @param $idVisiteur : l'id du visiteur 
 	 * @param $mois : le mois de la fiche à modifier
 	*/
 	public function voirFiche($idVisiteur, $mois)
@@ -70,30 +53,11 @@ class A_visiteur extends CI_Model {
 	}
 
 	/**
-	 * Présente le détail de la fiche sélectionnée et donne
-	 * accés à la modification du contenu de cette fiche.
-	 *
-	 * @param $idVisiteur : l'id du visiteur
-	 * @param $mois : le mois de la fiche à modifier
-	 * @param $message : message facultatif destiné à notifier l'utilisateur du résultat d'une action précédemment exécutée
-	*/
-	public function modFiche($idVisiteur, $mois, $message=null)
-	{	// TODO : s'assurer que les paramètres reçus sont cohérents avec ceux mémorisés en session
-
-		$data['notify'] = $message;
-		$data['numAnnee'] = substr( $mois,0,4);
-		$data['numMois'] = substr( $mois,4,2);
-		$data['lesFraisHorsForfait'] = $this->dataAccess->getLesLignesHorsForfait($idVisiteur,$mois);
-		$data['lesFraisForfait'] = $this->dataAccess->getLesLignesForfait($idVisiteur,$mois);
-
-		$this->templates->load('t_visiteur', 'v_visModListeFrais', $data);
-	}
-
-	/**
 	 * Signe une fiche de frais en changeant son état
 	 *
-	 * @param $idVisiteur : l'id du visiteur
-	 * @param $mois : le mois de la fiche à signer
+	 * @param $idVisiteur : l'id du comptable
+	 * @param $mois : Vois toutes les fiches à valider ce mois ci
+   *
 	*/
 	public function signeFiche($idVisiteur, $mois)
 	{	// TODO : s'assurer que les paramètres reçus sont cohérents avec ceux mémorisés en session
